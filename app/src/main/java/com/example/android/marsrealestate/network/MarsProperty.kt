48212c68c49a24a0fec0da48fc5805f707db7e9d
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-04-12 17:38:53 +0300

    Add @Json annotation to remap img_src JSON

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,9 +17,12 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Json
+
 data class MarsProperty(
         val id: String,
-        val img_src: String,
+        @Json(name = "img_src")
+        val imgSrcUrl: String,
         val type: String,
         val price: Double
 )
